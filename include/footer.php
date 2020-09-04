3d8c38cf9c6303b26fa5fc7988be6b32d522100e
icmsa <marvina@icmsaust.com.au>
2020-09-04 15:06:34 +1000

    Removed the sidebar sponsors when on the sponsorship page

--- include/footer.php
+++ include/footer.php
@@ -8,10 +8,11 @@
     <li><strong>Registration opens</strong> September 2020</li>
     <li class="event"><strong>AMTA 2020 Conference</strong> 7 - 15 November 2020</li>
   </ul>
-  <h2 class='mt-5'>AMTA 2020 Sponsors</h2>
-  <img src="./img/sponsors/sound-expression.png" class="img-fluid p-4">
-  <img src="./img/sponsors/mto.png" class="img-fluid p-4">
-
+  <?php if ($title != "Conference Sponsorship Opportunities") { ?>
+      <h2 class='mt-5'>AMTA 2020 Sponsors</h2>
+      <img src="./img/sponsors/sound-expression.png" class="img-fluid p-4">
+      <img src="./img/sponsors/mto.png" class="img-fluid p-4">
+  <?php } ?>
 </div>
 </div>
 </div>
