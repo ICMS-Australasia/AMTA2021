6598bf85e89730f72990ea712b424e134b0cb55f
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-09-23 16:49:30 +1000

    Added PDS Program

---
+++ pds-program.php
@@ -0,0 +1,55 @@
+<?php
+$title = "PDS Program";
+include('include/header.php');
+?>
+
+<h2>Saturday 7 November 2020</h2>
+
+<table cellpadding="20" cellspacing="0" border="1">
+    <col width="25%">
+    <col width="25%">
+    <col width="25%">
+    <col width="25%">
+    <tr>
+        <td align="center">10:00am – 10:15am</td>
+        <td colspan="3">Welcome including acknowledgement of country</td>
+    </tr>
+    <tr>
+        <td align="center">10:30am – 11:30am</td>
+        <td colspan="3">Vocal sustainability in change of climate <br>
+            <b>Associate Professor Debra Phyland</b></td>
+    </tr>
+    <tr>
+        <td align="center">11:30am – 11:45am</td>
+        <td colspan="3">Networking and coffee break</td>
+    </tr>
+    <tr>
+        <td align="center">11:45am – 12:45pm</td>
+        <td>Guitar Skills <br>
+            <b>Oliver O’Reilly</b></td>
+        <td>GIM principles and PD <br>
+            <b>Katrina McFerran</b></td>
+        <td>Music Tech <br>
+            <b>Rachael Mackay</b></td>
+    </tr>
+    <tr>
+        <td align="center">12:45pm – 1:30pm</td>
+        <td colspan="3">Lunch break</td>
+    </tr>
+    <tr>
+        <td align="center">1:30pm – 2:00pm</td>
+        <td colspan="3">Body Health/Stretching <br>
+            <b>Yves Silveira</b></td>
+    </tr>
+    <tr>
+        <td align="center">2:00pm – 4:30pm*</td>
+        <td colspan="3">Counselling Skills <br>
+            <b>Libby Sherring</b> <br>
+            <em>*Additional Breaks will be scheduled during this time</em></td>
+    </tr>
+</table>
+
+
+<?php
+include('include/footer.php');
+?>
