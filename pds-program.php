666158690887aa71856350028da1fff12d418c86
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2020-11-04 11:03:07 +1100

    Added resource/requirements

--- pds-program.php
+++ pds-program.php
@@ -28,11 +28,29 @@ include('include/header.php');
     <tr>
         <td align="center">11:45am – 12:45pm</td>
         <td>Unlocking melody on the guitar <br>
-            <b>Oliver O’Reilly</b></td>
+            <b>Oliver O’Reilly</b> <br><br>
+            Resources/Requirements
+            <ul>
+              <li>Guitar (tuned before workshop)</li>
+              <li>Tuner and capo</li>
+              <li>Zoom audio settings set to ‘Suppress Persistent Background Noise’</li>
+            </ul>
+        </td>
         <td>Musical insights: Identifying internal barriers to success and flourishing <br>
-            <b>Katrina Skewes McFerran</b></td>
+            <b>Katrina Skewes McFerran</b> <br><br>
+            Resources/Requirements
+            <ul>
+              <li>Invited to have drawing materials at hand (e.g. pastels and paper)</li>
+              <li>Invited to download Rebelle 3 drawing program</li>
+            </ul>
+        </td>
         <td>Let’s get Technical: Technology and Music Therapy <br>
-            <b>Rachael Mackay</b></td>
+            <b>Rachael Mackay</b> <br><br>
+            Resources/Requirements
+            <ul>
+              <li>Phone or device to capture QR codes that will be displayed during the session</li>
+            </ul>
+        </td>
     </tr>
     <tr>
         <td align="center">12:45pm – 1:30pm</td>
@@ -41,13 +59,27 @@ include('include/header.php');
     <tr>
         <td align="center">1:30pm – 2:00pm</td>
         <td colspan="3">How to stay pain free in music therapy <br>
-            <b>Yves Silveira</b></td>
+            <b>Yves Silveira</b> <br><br>
+            Resources/Requirements
+            <ul>
+              <li>Space to do exercises</li>
+              <li>Tennis ball or equivalent )lacross ball/spiky ball)</li>
+              <li>Theraband/strap if you have one</li>
+              <li>Comfortable clothes</li>
+            </ul>
+        </td>
     </tr>
     <tr>
         <td align="center">2:00pm – 4:30pm*</td>
         <td colspan="3">“Fostering Connection In The Therapeutic Relationship” - Counselling Skills <br>
             <b>Libby Sherring</b> <br>
-            <em>*Additional Breaks will be scheduled during this time</em></td>
+            <em>*Additional Breaks will be scheduled during this time</em> <br><br>
+            Resources/Requirements
+            <ul>
+              <li>In Webinar Workbook (sent to attendees via email)</li>
+              <li>Pre-webinar activities booklet (sent to attendees via email)</li>
+            </ul>
+        </td>
     </tr>
     <tr>
         <td align="center">4:30pm – 4:45pm</td>
