a88ee7ddcedfed7a6a46c8ae08fc3f6df6e93217
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-07-06 10:47:54 +1000

    Updated image

--- keynote-speakers.php
+++ keynote-speakers.php
@@ -3,7 +3,13 @@ $title = "Keynote Speakers";
 include('include/header.php');
 ?>
 
-<img src="./img/keynote/murphy.png" class="img-fluid float-right ml-4 mb-4" width="250">
+<picture>
+  <source media="(min-width: 576px)" srcset="./img/keynote/murphy.png" width="250">
+  <source media="(min-width: 768px)" srcset="./img/keynote/murphy.png" width="250">
+  <source media="(min-width: 992px)" srcset="./img/keynote/murphy.png" width="250">
+  <source media="(min-width: 1200px)" srcset="./img/keynote/murphy.png" width="250">
+  <img src="./img/keynote/murphy.png" class="img-fluid float-right ml-4 mb-4" width="150">
+</picture>
 <h2 class="mb-0">Dr Melissa Murphy</h2>
 <h3 class="mt-0">Dr Melissa Murphy will speak on music and music therapy in the disability sector; the current state of play and a bright future led by the raised voices of the disability community.</h3>
 <p class="mt-4">Dr Melissa Murphy, PhD, RMT is a music therapy practitioner, supervisor, educator and researcher. Since the early 1990s she has worked in mental health, aged and palliative care, but her primary interest lies in the disability sector. She currently works with both children and adults with disability in the education system, community settings and in private practice drawing on her skills as a clinical music therapist, GIM practitioner and community music therapist.  Her passion is in working with adolescents and young people with disability to ensure equal access to music, whether that is music for health and well-being, community music, music education or vocational pathways. She is the founder of MusicSpace, a community music hub for people living in the greater Geelong and Surf coast regions. Philosophically informed by critical and ecological theory, MusicSpace aims to foster personal and group creativity, as well as social and musical connections within the local community. She is the former National Disability Insurance Scheme (NDIS) representative and Disability Advisor for AMTA including the music therapy representative for the NDIS working group of Allied Health Professions Australia (AHPA).</p>
