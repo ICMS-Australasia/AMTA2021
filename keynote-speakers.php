49e508ae5ccb280f9531cc2ee502859641878264
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-07-28 11:12:30 +1000

    Added Keynote

--- keynote-speakers.php
+++ keynote-speakers.php
@@ -14,6 +14,19 @@ include('include/header.php');
 <h3 class="mt-0">Dr Melissa Murphy will speak on music and music therapy in the disability sector; the current state of play and a bright future led by the raised voices of the disability community.</h3>
 <p class="mt-4">Dr Melissa Murphy, PhD, RMT is a music therapy practitioner, supervisor, educator and researcher. Since the early 1990s she has worked in mental health, aged and palliative care, but her primary interest lies in the disability sector. She currently works with both children and adults with disability in the education system, community settings and in private practice drawing on her skills as a clinical music therapist, GIM practitioner and community music therapist.  Her passion is in working with adolescents and young people with disability to ensure equal access to music, whether that is music for health and well-being, community music, music education or vocational pathways. She is the founder of MusicSpace, a community music hub for people living in the greater Geelong and Surf coast regions. Philosophically informed by critical and ecological theory, MusicSpace aims to foster personal and group creativity, as well as social and musical connections within the local community. She is the former National Disability Insurance Scheme (NDIS) representative and Disability Advisor for AMTA including the music therapy representative for the NDIS working group of Allied Health Professions Australia (AHPA).</p>
 
+<hr class="mt-4">
+
+<picture>
+  <source media="(min-width: 576px)" srcset="./img/keynote/baines.png" width="250">
+  <source media="(min-width: 768px)" srcset="./img/keynote/baines.png" width="250">
+  <source media="(min-width: 992px)" srcset="./img/keynote/baines.png" width="250">
+  <source media="(min-width: 1200px)" srcset="./img/keynote/baines.png" width="250">
+  <img src="./img/keynote/baines.png" class="img-fluid float-right ml-4 mb-4" width="150">
+</picture>
+<h2 class="mb-0">Dr Sue Baines</h2>
+<h3 class="mt-0">Dr Sue Baines will discuss anti-oppressive music therapy: the foundations and the practice.</h3>
+<p class="mt-4">Dr Sue Baines, MTA, FAMI is a music therapy scholar/educator/practitioner and social justice activist. Because of her interest in how music is helpful for our human family, over the past three decades, she has worked with people of all ages in many settings, from youth corrections to long term care to in-patient and forensic psychiatry to at-risk children to community mental health. Her social justice focus has shaped her practice and research, first feminist music therapy in her master’s work, followed by community music therapy research, leading up to anti-oppressive music therapy. Since 1995, Sue has been a field supervisor for music therapy practicum students and since 1997, Sue has taught undergraduate music therapy students at Capilano University in North Vancouver, BC, Canada. Her combination of music therapy scholar/educator/practitioner keeps her research and teaching grounded in practice. Furthering her goal of social justice, Sue has authored many articles, chapters, and book reviews interrogating and contextualizing music therapy with the goal of ending systemic oppression in our field. She has been a member of both the Canadian Association of Music Therapists and Music Therapy Association of British Columbia boards and is past Editor-in-Chief of the Canadian Journal of Music Therapy. </p>
+
 <?php
 include('include/footer.php');
 ?>
