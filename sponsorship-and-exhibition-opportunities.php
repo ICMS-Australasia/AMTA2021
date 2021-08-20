0cb793e6f72d1820ee65bf7b7963bba6b92594fe
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-08-20 10:57:19 +1000

    Updated order

--- sponsorship-and-exhibition-opportunities.php
+++ sponsorship-and-exhibition-opportunities.php
@@ -3,21 +3,6 @@ $title = "Conference Sponsorship Opportunities";
 include('include/header.php');
 ?>
 
-<p>This year we look forward to partnering with individuals, small and large businesses and other organisations to support the music therapy community. The AMTA Conference will be delivered virtually again in 2021. The 2021 sponsorship opportunities offer exposure via the dedicated AMTA 2021 virtual conference platform. Sponsorship opportunities will range in price from only $250 upwards, and can be tailored to suit your budget and requirements.</p>
-<p>The generous benefits of sponsorship offer exposure to over 250 health professionals via the virtual conference platform and online via the website in the lead up to the event. The limited Platinum, Gold and Silver sponsorship packages will provide supporters exclusive benefits that will showcase their business or organisation, and will communicate their dedicated support of music therapy. These packages will allow supporters to be part of the virtual exhibition which offers exposure through branding, engagement and networking with 2021 attendees. The virtual exhibition will be accessible by attendees not only during the conference dates, but also for a period after the conference, therefore supporter exposure is maximised. Supporters have the opportunity to connect with attendees and swap contact details as well as receive analytics regarding attendee traffic and engagement.</p>
-<p>Conference sponsorship provides a unique opportunity to target your marketing to those who really need to see it. If an individual, a business, or an organisation you know of could benefit from partnering with AMTA this year, please forward them this information.</p>
-<p>We welcome applications for sponsorship from all interested individuals, small business owners, organisations, associations, groups and services.</p>
-<p>Please contact Catherine Vesic, Social, Marketing & Sponsorships Coordinator, about how you can be involved with the premier annual event for music therapists.</p>
-
-<h3 class="mb-2">Catherine Vesic</h3>
-<p>Social, Marketing & Sponsorships Coordinator <br>
-  Email: <a href="mailto:catherinevesic@gmail.com">catherinevesic@gmail.com</a></p>
-
-<p>Please find below a copy of the AMTA 2021 Sponsorship prospectus & application form</p>
-
-<a href="./AMTA2020-prospectus.pdf" class="button mt-4" target="_blank">Download Sponsorship Prospectus</a>
-
-
 <h2>AMTA 2021 Sponsors</h2>
 <h3>Silver Sponsor</h3>
 <div class="row align-items-center">
@@ -35,6 +20,24 @@ include('include/header.php');
     <a href="http://www.musictherapyonline.org" target="_blank"><img src="./img/sponsors/music-therapy-online.png" class="img-fluid"></a>
   </div>
 </div>
+
+<hr class="my-4">
+
+<p>This year we look forward to partnering with individuals, small and large businesses and other organisations to support the music therapy community. The AMTA Conference will be delivered virtually again in 2021. The 2021 sponsorship opportunities offer exposure via the dedicated AMTA 2021 virtual conference platform. Sponsorship opportunities will range in price from only $250 upwards, and can be tailored to suit your budget and requirements.</p>
+<p>The generous benefits of sponsorship offer exposure to over 250 health professionals via the virtual conference platform and online via the website in the lead up to the event. The limited Platinum, Gold and Silver sponsorship packages will provide supporters exclusive benefits that will showcase their business or organisation, and will communicate their dedicated support of music therapy. These packages will allow supporters to be part of the virtual exhibition which offers exposure through branding, engagement and networking with 2021 attendees. The virtual exhibition will be accessible by attendees not only during the conference dates, but also for a period after the conference, therefore supporter exposure is maximised. Supporters have the opportunity to connect with attendees and swap contact details as well as receive analytics regarding attendee traffic and engagement.</p>
+<p>Conference sponsorship provides a unique opportunity to target your marketing to those who really need to see it. If an individual, a business, or an organisation you know of could benefit from partnering with AMTA this year, please forward them this information.</p>
+<p>We welcome applications for sponsorship from all interested individuals, small business owners, organisations, associations, groups and services.</p>
+<p>Please contact Catherine Vesic, Social, Marketing & Sponsorships Coordinator, about how you can be involved with the premier annual event for music therapists.</p>
+
+<h3 class="mb-2">Catherine Vesic</h3>
+<p>Social, Marketing & Sponsorships Coordinator <br>
+  Email: <a href="mailto:catherinevesic@gmail.com">catherinevesic@gmail.com</a></p>
+
+<p>Please find below a copy of the AMTA 2021 Sponsorship prospectus & application form</p>
+
+<a href="./AMTA2020-prospectus.pdf" class="button mt-4" target="_blank">Download Sponsorship Prospectus</a>
+
+
 
 <?php
 include('include/footer.php');
