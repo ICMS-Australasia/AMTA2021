81afc7e6d5cb4e7f8b716eecaecb6ad60103fd3c
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-08-31 16:28:17 +1000

    Added speakers

--- spotlight-panel-speakers.php
+++ spotlight-panel-speakers.php
@@ -7,9 +7,7 @@ include('include/header.php');
 
 <p>Throughout history, Southeast Asia has been a melting pot of cultures, religions, food heritages, along with spiritual, healing, and music traditions. The few qualified SEA music therapists face unique opportunities and challenges as they juggle numerous professional plates. This special platform will bring together unique perspectives of how music therapists are bridging larger intersectional themes of healing, amplifying indigenous music roots, and advocating for the profession within their own east-west situatedness in clinical practice, across the intersections of Malaysia, Thailand, Indonesia, and Singapore.</p>
 
-<hr class='my-4'>
-
-<div class="row">
+<div class="row mb-5">
     <div class="col">
         <img src="./img/speakers/kwan.png" class="img-fluid float-right ml-4" width="200">
         <h3 class="mt-0">Melanie Kwan – Singapore</h3>
@@ -17,9 +15,7 @@ include('include/header.php');
     </div>
 </div>
 
-<hr class='my-4'>
-
-<div class="row">
+<div class="row mb-5">
     <div class="col">
         <img src="./img/speakers/kalsi.png" class="img-fluid float-right ml-4" width="200">
         <h3 class="mt-0">Gurpreet Kaur Kalsi - Malaysia</h3>
@@ -27,9 +23,7 @@ include('include/header.php');
     </div>
 </div>
 
-<hr class='my-4'>
-
-<div class="row">
+<div class="row mb-5">
     <div class="col">
         <img src="./img/speakers/hariwijaya.png" class="img-fluid float-right ml-4" width="200">
         <h3 class="mt-0">Jessica Hariwijaya – Indonesia</h3>
@@ -37,15 +31,45 @@ include('include/header.php');
     </div>
 </div>
 
-<hr class='my-4'>
-
-<div class="row">
+<div class="row mb-5">
     <div class="col">
         <img src="./img/speakers/pakdeesatitwara.png" class="img-fluid float-right ml-4" width="200">
         <h3 class="mt-0">Napak Pakdeesatitwara – Thailand</h3>
         <p class="mt-4">RMT, Neurologic Music Therapist, is a PhD student at the University of Melbourne and currently working as a lecturer in music therapy at Chulalongkorn University in Bangkok, Thailand. He has experience working in developmental health, aged care, neurorehabilitation, and health tech.</p>
     </div>
 </div>
+
+<hr class='mt-4 mb-5'>
+
+<h2>The Evolving Symphonies of Life: Conducting Music Therapy in Cancer and Palliative Care across the Lifespan</h2>
+
+<p>Being diagnosed with a life-threatening illness is life-changing for the individual and their family. Music therapy can do much in this space, integrating moments of peace and comfort into treatment and care, amplifying voices of stability and familiarity, strengthening bonds and connection, and transforming experiences of fear and sadness into moments of joy and fun. The Panel will explore how music therapy can amplify and evolve the resilience and strengths of the patient and family, support wellbeing and living fully, facilitate moments of sharing and support, and provide enduring legacies. </p>
+
+<div class="row mb-5">
+    <div class="col">
+        <img src="./img/speakers/schreck.png" class="img-fluid float-right ml-4" width="200">
+        <h3 class="mt-0">Brian Schreck</h3>
+        <p class="mt-4">Brian Schreck, MA, MT-BC is a board-certified music therapist who has been serving the chronically ill since 2004. Brian has a bachelor’s of arts in music therapy from Berklee College of Music and a master’s of arts in music therapy from New York University. Palliative care, end-of-life care, and bereavement are areas of passion for Mr. Schreck. Brian has presented locally, nationally, and internationally on his innovative work in medical music therapy. Schreck pioneered the use of heartbeat recordings as a way to rhythmically connect with patients and their families through ongoing recording projects. This rhythmic stem cell is something that can be worked on, changed, and transformed into music forever. Visit <a href="http://www.amplifiedcpr.org" target="_blank">www.amplifiedcpr.org</a> for more information. Brian Schreck’s main aim as a medical music therapist is to be as creative as possible to serve the needs of the patients he serves as well as their families. Brian Schreck facilitates ongoing in and outpatient music therapy with people with a diagnosis of cancer through the Norton Cancer Institute. </p>
+    </div>
+</div>
+
+<div class="row mb-5">
+    <div class="col">
+        <img src="./img/speakers/mcleod.png" class="img-fluid float-right ml-4" width="200">
+        <h3 class="mt-0">Roxanne McLeod</h3>
+        <p class="mt-4">Roxanne is a Registered Music Therapist in Sydney Australia who specialises in working with children and families in a range of settings, including paediatric oncology and palliative care at The Children’s Hospital at Westmead. She has a passion for family-centred care, facilitating experiences that help children and families reach their fullest potential, and create meaningful moments of connection through music.</p>
+    </div>
+</div>
+
+
+<div class="row mb-5">
+    <div class="col">
+        <img src="./img/speakers/james.png" class="img-fluid float-right ml-4" width="200">
+        <h3 class="mt-0">Maggie James (nee Leung)</h3>
+        <p class="mt-4">Maggie is a music therapist with 18 years’ experience in paediatric healthcare and has worked extensively in intensive care and palliative care.  She is the former Team Leader of Allied Health Oncology Services and the Music Therapy Department at the Queensland Children’s Hospital (QCH). Maggie was an invited speaker at this year’s TEDx Talk UQ Brisbane, and shared her professional and personal journey in paediatric palliative care.  She volunteers in the area of music therapy and palliative care internationally, and is a board member of the Butterfly Children’s Hospice in China and a professional consultant for the International Music Therapy Centre in Hong Kong.  In 2014, Maggie was recognised for her community contribution and awarded The University of Queensland MBA Service to the Community Award.  Maggie is passionate about using music to support children and family in palliation and believes that like birth, the death of a child should be with dignity and love.  </p>
+    </div>
+</div>
+
 
 <?php
 include('include/footer.php');
