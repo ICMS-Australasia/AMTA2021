1684ff0e833efc4cfa8949dc3d1131a7b305faea
Marvin Agbuya <marvinjohnagbuya@gmail.com>
2021-09-15 10:38:57 +1000

    Added pds speakers

--- pds-speakers.php
+++ pds-speakers.php
@@ -25,7 +25,7 @@ include('include/header.php');
       name: "Dr Andrew Knight",
       workshop: "Early Childhood Music Therapy Refresh: Songs, Interventions, and Theories",
       content: `Dr. Andrew Knight holds a bachelor's degree in Percussion Performance, Jazz emphasis from University of Wisconsin-La Crosse, a music therapy equivalency and master's degree from the University of Minnesota, and a Ph.D. in Educational Foundations and Research from the University of North Dakota. Dr. Knight has research interests in substance use disorders in adults and early childhood social/emotional developmental issues. He directs the Parkinson’s Disease vocal exercise group in collaboration with the Parkinson’s Support Group in Larimer County and is a Music Together Within Therapy provider. His research has been published in the Journal of Music Therapy and Music Therapy Perspectives, among other journals, and serves as Associate Editor for Book Reviews for JMT. He is a past president of the Midwestern Region (MWR) of the American Music Therapy Association (AMTA), on the AMTA Assembly of Delegates, and has won the MWR and AMTA Service Awards. Along with Drs. Blythe LaGasse and Alicia Clair, he is a co-editor of the 2018 Introduction to Music Therapy textbook published by AMTA.`,
-      about: '',
+      about: 'This PDS will include an introduction to early childhood music therapy approaches, stimulate your creativity for session planning arcs and contours, and provide ideas for parent/caregiver engagement. Learn a few songs, strategies and sing along from home, with an opportunity for Q&A and interaction during this live workshop.',
       video: 'https://player.vimeo.com/video/579225410',
     },
     {
@@ -45,7 +45,7 @@ include('include/header.php');
       name: "Kylie Morgan (PhD, GDip Music Therapy, BCA)",
       workshop: "Music Therapy NDIS Report Writing",
       content: `Kylie has works as a senior consultant for Disability Services Consulting. She completed her PhD in Music Therapy in 2007. During her career as a Registered Music Therapist she gained extensive experience in designing, implementing and evaluating music therapy programs in the mental health, aged and disability sectors. For the past 7 years, she has devoted her career to the disability sector and it’s interface with other systems. She has developed a deep knowledge and expertise in NDIS and it’s place within the allied health and mental health sectors. This knowledge is combined with her personal passion for the sector arising out of her lived experience as a proud mum of a daughter with ASD, who is also an NDIS participant.`,
-      about: ``,
+      about: "It's no secret that the strength of an allied health professional’s report can make or break an NDIS plan. This workshop will provide an overview of NDIS reporting requirements, how to write reports to meet NDIS funding responsibilities, key terms to avoid, examples of language to use when summarising supports provided, and how to structure your report for clarity. Participants will also receive an NDIS progress report template.",
       video: '',
     },
     {
@@ -55,7 +55,9 @@ include('include/header.php');
       name: "Kate Fletcher",
       workshop: "Thinking outside the Music; Creative ways to work with adults with disabilities",
       content: `Kate Fletcher is an RMT/NMT with clinical experience working with children and adults in a variety of clinical settings (special education, disability, early intervention, ABI and community).  Kate is passionate about using a client-centred, holistic approach to her work, and is currently the manager of clinical services for Music Beat Australia.`,
-      about: ``,
+      about: `
+      When working in the adult disability field, the needs and goals of our clients can differ widely across our client base.  It can be a bit of a minefield to perfect the balance of our clients’ own musical goals, functional outcomes that need to be reported to funding bodies, and our own creativity and skills as therapists.  But!!!  When working in the community space, we are only limited by our own creativity and the creativity of our clients. <br><br>
+      This workshop will practically explore some different and creative ways to work with adults with disabilities through a music therapy program.  It will touch on group and individual programs, different interventions, and a variety of creative forms of self-expression that enable adults with disabilities to reach their own personal goals.`,
       video: '',
     },
     {
@@ -76,6 +78,65 @@ include('include/header.php');
       workshop: "Expanding, integrating and evolving your assessment skills in music therapy. ",
       content: `Gustavo Schulz Gattino, Ph.D, is music therapist and Associate Professor in the Department of Communication and Psychology at the Aalborg University (Denmark). He is a teacher of the Bachelor, Master and PhD music therapy programs at the same university. He is accredited as a music Therapist by the Portuguese Association of Music Therapy (APMT) and the country representative of Denmark in the European Music Therapy Confederation (EMTC). Dr. Gattino is the editor of publications Brazilian Journal of Music Therapy and Portuguese Journal of Music Therapy (RPM). He is a member of the International Music Therapy Evaluation Consortium (IMTAC) and also member of the Publications commission of the World Federation of Music Therapy (WFMT).`,
       about: `This workshop aims to present strategies and knowledge that can help music therapists in their assessment practices. This proposal is based on theoretical discussions and practical experiences according to the four basic steps of the music therapy assessment process: 1) preparation, 2) information gathering, 3) analysis, interpretation, and conclusions, and 4) documentation and communication of assessment results. In addition, the course will address practical strategies on how to select or combine different assessment methods and tools based on the needs of each context/population in music therapy`,
+      video: '',
+    },
+    {
+      title: "",
+      firstname: "John",
+      lastname: "Carpente",
+      name: "John Carpente",
+      workshop: "Musical Goals are Clinical Goals: Hearing Music as a Domain of Health",
+      content: `John A. Carpente, Ph.D., MT-BC, LCAT is a Professor of Music and Music therapy at Molloy College, Founder, and Director of The Rebecca Center for Music Therapy at Molloy College, NY, USA, Founding Clinical Director of the Center for Autism and Child Developmental and owner of Developmental Health Services. He has over 20 years of music therapy clinical, supervisory, and research experience and has presented his work domestically and internationally. He is a Board Certified and New York State Licensed music therapist as well as a certified DIRFloortime practitioner and trainer, and creator of the Developmental Relationship-based Music Therapy model. He is a founding member of the International Music Therapy Assessment Consortium (IMTAC) and author of the Individual Music-Centered Assessment Profile for Neurodevelopmental Disorder assessment model. He has published numerous book chapters and peer-reviewed journal articles on the topic of music therapy with autistic individuals.`,
+      about: `Generally, in most music therapy practices, conventional heath domain areas such as physical, cognitive, social, and speech are the therapist’s primary clinical focus. Hence, music is then used as a means for a non-musical end, and therefore goal plans, reports, as well as our identity as a discipline are guided by conventional domains rather than music domains. This presentation will introduce the idea of music as a domain area of health in which it is viewed (and heard) as a means to its own end and clinical goals and health are understood musically.`,
+      video: '',
+    },
+    {
+      title: "",
+      firstname: "",
+      lastname: "Sujono-Devlin",
+      name: "Iani Sujono & Rob Devlin",
+      workshop: "Helping each other to raise the bar in the MT Profession in Private Practice",
+      content: `Iani Sujono and Rob Devlin are a husband and wife team with extensive experience in music therapy, disability and private practice. They started their business Sound Expression in 2015 and have grown this into a successful clinical practice with 250 clients and 13 staff. Iani initially worked within community settings (aged care, schools and day programs) before becoming the Operations Manager at Nordoff-Robbins Music Therapy Australia for 13 years. She then pursued her dream to open up her own music therapy clinic. Rob came from a strong business background in the corporate sector for 18 years before changing direction and pursuing a music therapy career. He joined Nordoff-Robbins Music Therapy Australia as a music therapist and later became the Head of Business Development for 5 years before joining Iani at Sound Expression. Between both of them, they have solid backgrounds and experience in clinical practice, establishing and growing a private practice and navigating the many challenges of a rapidly changing business environment.`,
+      about: `
+        We are delighted that more RMTs are opening music therapy clinics around Australia! Over the last few years, we can see that this is having a positive impact on the awareness and credibility of the profession. It is important that we continue to help each other to raise the bar of our profession in <br><br>
+        Private Practice. We’d like to share our key learnings and invite open discussion of various topics including Advocacy and Collaboration, Business Strategy, Adaptability, and Quality Assurance.`,
+      video: '',
+    },
+    {
+      title: "",
+      firstname: "",
+      lastname: "Cameron-Mackney-Dalmazzo-Connell",
+      name: "Helen Cameron, Elizabeth Mackney, Megan Dalmazzo & Anita Connell",
+      workshop: "Connect, Create, Communicate: An interactive panel/workshop to keep RMTs up to date with information and resources provided by the AMTA on disability and the NDIS",
+      content: `
+        Helen Cameron established her private practice, JAM music therapy in Melbourne over 20 years ago and has been a registered provider since 2016. She manages a team of wonderful music therapists who provide individual and group programs to NDIS participants  Helen is completing her Master's by research at the University of Melbourne, focussing on the experiences of support workers who attend group music therapy. <br><br>
+        Elizabeth Mackney, BN MMusThy, RN RMT NMT, runs her own private practice, Lismore Music Therapy, in the Northern Rivers area of NSW.   Liz has been a Registered NDIS Provider since the scheme rolled out in the area and supports up to 50 NDIS participants of all ages through individual, family, and group music therapy programs. <br><br>
+        Megan Dalmazzo runs a private music therapy practice, Thrive Music, in the Newcastle/Hunter region of NSW. Megan has experience in early childhood early intervention, disability, dementia, and palliative care. She has operated as a Registered NDIS Provider since 2013. <br><br>
+        Anita Connell manages her own private practice, Tuned In Music Therapy, facilitating music therapy programs for disabled children and adults. Anita is also completing her PhD at the MARCS Institute, Western Sydney University researching the impact of creative music learning for older adults with and without cognitive impairment.
+      `,
+      about: `
+      In the ever-changing landscape of disability, and with continuous changes to the NDIS, it is vital that information communicated by the AMTA is current and accurate. <br><br>
+      As part of the AMTA’s Advocacy Plan, the Disability Working Group is revising and improving AMTA’s resources on music therapy in the NDIS. This is an ongoing process intended to provide AMTA members and other stakeholders with up-to-date information about music therapy and disability. <br><br>
+      During this panel/workshop we will outline the AMTA's Advocacy Plan for further strengthening the position of the music therapy profession in the NDIS.  We will examine and seek critique on the relevance of current AMTA NDIS resources and their effectiveness in supporting members working in the Scheme.  We will also examine and seek critique on the relevance of our existing resources and information that is intended for key stakeholders other than AMTA members e.g., NDIA staff who approve music therapy in NDIS plans, NDIS participants, and other NDIS providers.  We are also eager to discuss ideas regarding the best pathways for dissemination of this information, as well as learning more about common challenges experienced by RMTs working in the sector.  A key feature of the AMTA Advocacy Plan is the engagement and involvement of all RMTs who work in the NDIS.  We believe this panel discussion/workshop is an important way for us all to come together and strengthen the position of music therapy in the NDIS now and into the future.
+      `,
+      video: '',
+    },
+    {
+      title: "",
+      firstname: "",
+      lastname: "Hall-Lseli",
+      name: "Christo Hall & Franziska Lseli",
+      workshop: "How to stand out from the crowd (branding, unique selling point); how to develop a one-page marketing plan; ensure your online eco-system is working for you to optimise lead conversions",
+      content: `
+        Christo is co-founder of Basic Bananas, Australia’s leading marketing mentoring organization for business owners. Christo is regularly featured in a range of media for his creative marketing and business ideas including: The Today Show (Channel Nine live TV), a regular on radio including 2UE and 2GB radio, Virgin Inflight Entertainment, TEDx and many more. Back in the day Christo was a professional surfer for 7 years competing on the international circuit. He is co-author of 2 number one international bestselling marketing books ‘Perception’ and ‘Bananas About Marketing’ and has presented on business stages all around planet earth. Christo has developed a reputation for assisting to add millions in additional revenue to his client’s bottom line. <br><br>
+        Franziska is the co-founder of <a href="https://www.basicbananas.com/" target="_blank">Basic Bananas</a>, <a href="https://www.thebusinesshood.com/" target="_blank">The Business Hood</a>, <a href="https://oceanlovers.com/" target="_blank">Oceanlovers</a> and <a href="https://www.momentsofhumanity.com/" target="_blank">Moments of Humanity</a>. In 2013 Franziska was awarded the Young Entrepreneur of the Year award recognizing her innovation, creativity and philanthropic involvement. Franziska is a board member at the Entrepreneurs Organization, sits on the judging panel for Singularity University and has run think tanks at the United Nations to address the UN sustainable development goals through entrepreneurship. Franziska is the author of two bestselling books with a third one just released this year (Sir Richard Branson wrote the foreword).   She is regularly featured across the media including INC, Forbes, Business Insider, Virgin Inflight Entertainment, Marie-Claire, Cosmopolitan, SmartCompany, Dynamic Business Magazine, BRW, 2UE, Channel 9. Franziska is a Swiss born Australian (or Swaussie) and is known for her down-to-earth attitude, infectious energy, integrity and courage to take the lead.
+      `,
+      about: `
+      Learn clever ways to grow your business and get more clients. If you would love a simple process to create effective marketing, which delivers results in the form of new clients, then this workshop is a must. The focus of this online workshop is to assist busy therapists including business owners and contractors to not only select the right marketing channels but also generating better response rates from everything you do.<br><br>
+      In this 2-hour online workshop, you will discover:<br>
+       <ul><li>How to stand out as a music therapist</li><li>How to create an effective one-page marketing plan</li><li>How to select the right marketing strategies for your business (big and small)</li><li>How to get a better return from all marketing efforts.</li><li>How to build an effective online marketing machine that runs for you 24/7.</li><li>The four most important marketing funnels every business must-have.</li></ul>
+      This workshop will take the confusion out of all your marketing efforts and show you how to gain more control when it comes to growing your business sustainably
+      `,
       video: '',
     },
   ];
